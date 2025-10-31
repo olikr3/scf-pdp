7649d921e08028bcc82513e41a8ccd18fce5ff7e
olikr3 <oliver.kremser@gmail.com>
2025-10-31 13:08:23 +0100

    random solver

---
+++ src/solver/construction/random.rs
@@ -0,0 +1,15 @@
+use crate::{Instance, Solution, solver::construction::ConstructionHeuristic};
+
+
+// note to self: i think i need lifetimes here
+pub struct RandomConstruction {
+    instance: Instance,
+}
+
+
+impl RandomConstruction {
+    
+    fn from_instance(instance: Instance) -> Self {
+        todo!();
+    }
+}
