ec4138a4de4122328e60832300a4b7c70da2a0c6
olikr3 <oliver.kremser@gmail.com>
2025-11-07 10:15:08 +0100

    remove name() from solver

--- src/solver.rs
+++ src/solver.rs
@@ -3,7 +3,4 @@ use crate::{Instance, Solution};
 pub trait Solver {
     /// Solve the instance and return a solution
     fn solve(&self) -> Solution;
-    
-    /// Get the name of the solver
-    fn name(&self) -> &str;
 }
