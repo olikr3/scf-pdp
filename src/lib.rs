7a0631704f85d34a0e2f160fa080de66e3e5a21b
olikr3 <oliver.kremser@gmail.com>
2025-11-09 11:23:11 +0100

    added modules

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,12 @@ pub mod instance;
 pub mod solution;
 pub mod solver;
 pub mod deterministic;
+pub mod random;
+pub mod beam_search;
 
 pub use instance::Instance;
 pub use solution::Solution;
 pub use solver::Solver;
 pub use deterministic::DeterministicConstruction;
+pub use random::RandomConstruction;
+pub use beam_search::BeamSearch;
