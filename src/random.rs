67c5aa95e344bc3edab5785dadf11fca269781eb
olikr3 <oliver.kremser@gmail.com>
2025-11-10 21:49:21 +0100

    changed solver signature

--- src/random.rs
+++ src/random.rs
@@ -1,18 +1,23 @@
 use crate::{Instance, Solution, Solver};
 
-
+/*
+Samples gamma requests at random and assigns them to vehicles uniformally random if they have spare capacity.
+Can be biased towards routes closer to the depot.
+*/
 pub struct RandomConstruction {
     instance: Instance,
+    biased: bool,
 }
 
 
 impl RandomConstruction {
     
-    fn new(instance: Instance) -> Self {
-        Self { instance }
+    fn new(instance: Instance, biased: bool) -> Self {
+        Self { instance, biased }
     }
 
     fn construct_solution(&self) -> Solution {
+    
         todo!()
     }
 }
@@ -20,9 +25,5 @@ impl RandomConstruction {
 impl Solver for RandomConstruction {
     fn solve(&self) -> Solution {
         self.construct_solution()
-    }
-
-    fn name(&self) -> &str {
-        "Random Solution"
     }
 }
